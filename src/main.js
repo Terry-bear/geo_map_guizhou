2273e4b4c4ad030268901f5b531f3c55698806c1
Terry-zh <496971418@qq.com>
2019-04-22 15:54:28 +0800

    add axios

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
+import './plugins/axios'
 import App from './App.vue'
+import echarts from 'echarts'
+import axios from 'axios'
+
 
 Vue.config.productionTip = false
+
+Vue.prototype.$echarts = echarts
 
 new Vue({
   render: h => h(App),
