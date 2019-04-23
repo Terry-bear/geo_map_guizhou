e17d79602f8f679c14dd09169030b8b2dd3fb3b9
Terry-zh <496971418@qq.com>
2019-04-23 15:50:55 +0800

    [fix](3d): 修复鼠标hover效果

--- src/components/3Dmap.vue
+++ src/components/3Dmap.vue
@@ -23,19 +23,29 @@ export default {
   },
   data() {
     return {
-      chart: null
+      chart: null,
+      autoRotate: true,
+      maxBeta: 360,
+      controlArgs:{
+        maxBeta: 360,
+        autoRotate: true
+      }
     }
   },
   created() {
-    this.init()
+    this.init(this.controlArgs)
+    setInterval(() => {
+      this.controlArgs.maxBeta = this.maxBeta = (this.maxBeta/360 + 3) *360
+      this.init(this.controlArgs)
+    }, 20000)
   },
   methods: {
-    init() {
+    init(args) {
       this.$axios({
         url: '/guizhou.json',
         dataType: 'json'
       }).then((res) => {
-        let min = 0, max = 15;
+        const _this = this
         this.$echarts.registerMap('guizhou', res.data)
         this.chart = this.$echarts.init(this.$refs.geoMap)
         this.chart.setOption({
@@ -50,23 +60,13 @@ export default {
           groundPlane: {
             show: true
           },
-          visualMap: {
-            show: false,
-            min: min,
-            max: max,
-            inRange: {
-              color: ['#313695', '#4575b4', '#74add1', '#abd9e9', '#e0f3f8', '#ffffbf', '#fee090', '#fdae61', '#f46d43', '#d73027', '#a50026']
-            },
-            calculable: true
-
-          },
           geo3D: {
             map: 'guizhou',
             roam: true,
             itemStyle: {
               color: '#313569',
               opacity: 1,
-              borderWidth: 0.9,
+              borderWidth: 2.2,
               borderColor: '#86AEED'
             },
             label: {
@@ -76,20 +76,35 @@ export default {
                 fontSize: 9,
                 opacity: 1,
                 backgroundColor: 'rgba(0,0,0,0)'
-                //backgroundColor: 'rgba(53,171,199,0)'
               },
+              formatter: function (params) {
+                // 鼠标hover以后是emphasis
+                // 移开后是normal
+                if (params.status === 'emphasis') {
+                  _this.selectedAreaFunc(params.name)
+
+                }
+                else ''
+                return params.name
+              }
             },
             emphasis: { //当鼠标放上去  地区区域是否显示名称
               label: {
                 show: true,
                 textStyle: {
-                  color: '#fff',
+                  color: '#F8E71C',
                   fontSize: 16,
                   backgroundColor: 'rgba(0,23,11,0)'
                 }
-              }
+              },
+              itemStyle: {
+                color: '#5A5B68',
+                opacity: 0.9,
+                borderWidth: 0.9,
+                borderColor: '#86AEED'
+              },
             },
-            //shading: 'lambert',
+            shading: 'lambert',
             light: { //光照阴影
               main: {
                 color: '#fff', //光照颜色
@@ -103,59 +118,26 @@ export default {
               ambient: {
                 intensity: 0.3
               }
+            },
+            // 地图角度视图
+            viewControl: {
+              autoRotate: args.autoRotate, // 是否旋转
+              minBeta: -360, // 旋转最小角度
+              maxBeta: args.maxBeta, // 旋转最大角度
+              minAlpha: 30, // 上下旋转最小角度
+              maxAlpha: 30, // 上下旋转最大角度
+              zoomSensitivity: 0, // 缩放
+              autoRotateAfterStill: 5 // 鼠标静止后几秒旋转
             }
           }
-          // series: {
-          //   name: '贵阳',
-          //   type: 'map3D',
-          //   map: 'guizhou',
-          //   // data: [],
-          //   // regionHeight: 2,
-          //   // boxWidth: 70,
-          //   //boxHeight:50,
-          //   // boxDepth: 50,
-          //   top: '-10%',
-          //   //left:'10%',
-
-
-          //   // label: {
-          //   //   show: true,
-          //   //   formatter: function (params) {
-          //   //     let content = '';
-          //   //     content = params.name;
-          //   //     return content;
-          //   //   },
-          //   //   textStyle: {
-          //   //     color: '#EECBAD',
-          //   //     fontWeight: 'normal',
-          //   //     fontSize: 5,
-          //   //     backgroundColor: 'rgba(0,23,11,0)'
-          //   //   },
-
-
-          //   //   emphasis: {//对应的鼠标悬浮效果
-          //   //     show: true,
-          //   //     textStyle: { color: "#f00" }
-          //   //   }
-          //   // },
-          //   // itemStyle: {
-
-          //   //   normal: {
-
-          //   //     borderWidth: 0.4
-          //   //   }, //阴影效果
-          //   //   emphasis: {
-          //   //     color: 'rgb(255,255,255)'
-          //   //   }
-          //   // },
-
-          //   // viewControl: {
-          //   //   autoRotate: false,
-          //   //   distance: 70
-          //   // }
-          // }
         })
       })
+    },
+    /**
+     * @todo 地区选择后填写的逻辑
+     */
+    selectedAreaFunc(name) {
+      console.log('area\'s name:', name)
     }
   }
 }
